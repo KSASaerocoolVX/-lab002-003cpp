2d24bf160294aa6861966c60211602c02ea10918
KSASaerocoolVX <arseni.yusupov.07@gmail.com>
2025-12-04 21:35:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,19 +17,17 @@ void inputData(float* p, int n) {
     }
 }
 
-long double average(const float* p, int n) {
-    long double average=0;
+float Average(const float* p, int n) {
+    float average=0;
     int len=n;
     while (n--) {
         average+=*p;
         p++;
     }
     return average/len;
-
 }
 float minValue(const float* p, int n) {
     float min=*p;
-    int len=n;
     while (n--) {
         if (min>*p) {
             min=*p;
@@ -41,7 +39,6 @@ float minValue(const float* p, int n) {
 
 float maxValue(const float* p, int n) {
     float max=*p;
-    int len=n;
     while (n--) {
         if (max<*p) {
             max=*p;
@@ -53,22 +50,16 @@ float maxValue(const float* p, int n) {
 float* filterAboveAverage(const float* p, int n, int & newCount) {
     float *filter= new float[n];
     float *start_f= filter;
-    long double average=0;
-    int len=n;
+    float average= Average(p, n);
     while (n--) {
-        average+=*p;
-        p++;
-    }
-    p--;
-    average=average/len;
-    while (len--) {
         if (average<*p) {
             newCount++;
             *filter=*p;
             filter++;
         }
-        p--;
+        p++;
     }
+
     return start_f;
 }
 int main()
@@ -78,25 +69,30 @@ int main()
     cout << "=== Анализ сенсора  ==="<<'\n';
     cout << "Введите количество измерений: " ;
     cin >> n;
-    while (cin.fail()) {
+    while (cin.fail() || n<=0) {
         cout << "Неверное значение";
         cin.clear();
-        cin.ignore();
+        cin.ignore(1000);
         cin >> n;
     }
     cout << "Введите значения:";
     float* p = new float[n];
     inputData(p, n);
-    cout << "Среднее значение:" <<average(p, n) <<"\n";
+    cout << "Среднее значение:" <<Average(p, n) <<"\n";
     cout << "Минимум:"<< minValue(p, n) << '\n';
     cout << "Максимум" <<maxValue(p, n) << endl;
     float* filter= filterAboveAverage(p, n, newcount);
     float * temp_filter=filter;
-    cout << "Значения выше среднего:";
+    if (newcount==0) {
+        cout << "Значения равны" << endl;
+    }
+    else
+    {
+        cout << "Значения выше среднего:" << endl   ;
     while (newcount--) {
         cout <<*temp_filter << endl;
         temp_filter++;
-
+    }
     }
     delete[] p;
     delete[] filter;
